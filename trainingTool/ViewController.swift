5481c5f18e61c8fc986a35cf6a537d14d1dd385d
Brian Tolkin <briantolkin@ralphcossmbp219.lan>
2017-05-16 14:05:59 -0700

    Initial Commit

---
+++ trainingTool/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  trainingTool
+//
+//  Created by Brian Tolkin on 5/16/17.
+//  Copyright © 2017 Brian Tolkin. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
